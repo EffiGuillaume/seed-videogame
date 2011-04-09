542e9ae975a5ee42fb5b752abac5f6c1266c9334
guiguiduj@gmail.com <guiguiduj@gmail.com@02c11bdb-3b26-27e6-09f1-825876d00b7d>
2011-04-09 17:28:07 +0000

    Ajout des classes Entity et Component dans le nouveau package seed.engine

---
+++ trunk/src/seed/engine/RenderComponent.java
@@ -0,0 +1,22 @@
+package seed.engine;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.state.StateBasedGame;
+
+/**
+ * @author Guillaume
+ * Simple Component qui permet d'implémenter la méthode render().
+ * voir : {@link http://slick.cokeandcode.com/wiki/doku.php?id=entity_tutorial}
+ *
+ */
+public abstract class RenderComponent extends Component {
+
+	public RenderComponent(String id)
+	{
+		this.id = id;
+	}
+	
+	public abstract void render(GameContainer gc, StateBasedGame sb, Graphics gr);
+
+}
