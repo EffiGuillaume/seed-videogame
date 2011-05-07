6dce8de21635d39eb38f127a6fca9ed5949c4039
guiguiduj@gmail.com <guiguiduj@gmail.com@02c11bdb-3b26-27e6-09f1-825876d00b7d>
2011-05-07 14:56:22 +0000

    Ajout des classes Flower et Herb, ainsi que leur configurations respectives. Implémentation du système de config/evolution (reste à tester si les evolutions fonctionnent). Implémentation de quelques animations (graine qui pousse) et remaniement du render de Absorber.

---
+++ src/seed/units/Flower.java
@@ -0,0 +1,69 @@
+package seed.units;
+
+import java.util.ArrayList;
+
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+
+import seed.config.FlowerConfig;
+import seed.field.BlockType;
+import seed.interactions.Evolution;
+
+public class Flower extends Plant {
+	
+	public Flower(String id) throws SlickException {
+		super(id);
+		this.delay = Integer.parseInt(FlowerConfig.getProperty("LVL1_DELAY"));
+		this.range = Integer.parseInt(FlowerConfig.getProperty("LVL1_RANGE"));
+		this.storage = 0;
+		this.energy = 0;
+		this.energyDelay = Integer.parseInt(FlowerConfig.getProperty("LVL1_ENERGYDELAY"));
+		this.cost = Integer.parseInt(FlowerConfig.getProperty("LVL1_COST"));
+		this.waterEvolQty = Integer.parseInt(FlowerConfig.getProperty("LVL1_WATEREVOLQTY"));
+		this.airProd = Integer.parseInt(FlowerConfig.getProperty("LVL1_AIRPROD"));
+		this.waterRegressQty = Integer.parseInt(FlowerConfig.getProperty("LVL1_WATERREGRESSQTY"));
+		this.energyRegressQty = Integer.parseInt(FlowerConfig.getProperty("LVL1_ENERGYREGRESSQTY"));
+		this.imageReference = Integer.parseInt(FlowerConfig.getProperty("LVL1_IMAGEREFERENCE"));
+		
+		ArrayList<Evolution> evolutions = new ArrayList<Evolution>();
+			Evolution lvl1 = new Evolution("LVL1_Flower_Evolution");
+			lvl1.setEnergyCost(Integer.parseInt(FlowerConfig.getProperty("LVL1_EVOLCOST")));
+			ArrayList<BlockType> fields0 = new ArrayList<BlockType>();
+				fields0.add(BlockType.FORET);
+			lvl1.setFields(fields0);
+			lvl1.setType("LVL1");
+		
+			Evolution lvl2 = new Evolution("LVL2_Flower_Evolution");
+				lvl2.setEnergyCost(Integer.parseInt(FlowerConfig.getProperty("LVL2_EVOLCOST")));
+				ArrayList<BlockType> fields = new ArrayList<BlockType>();
+					fields.add(BlockType.FORET);
+				lvl2.setFields(fields);
+				lvl2.setType("LVL2");
+				
+			Evolution lvl3 = new Evolution("LVL3_Flower_Evolution");
+				lvl3.setEnergyCost(Integer.parseInt(FlowerConfig.getProperty("LVL3_EVOLCOST"))); 
+				ArrayList<BlockType> fields2 = new ArrayList<BlockType>();
+					fields2.add(BlockType.FORET);
+				lvl3.setFields(fields2);
+				lvl3.setType("LVL3");
+		
+		this.evolutions = evolutions;
+		
+		addComponent(new PlantRenderComponent(id+"_Render", new Image("res/graineSprite.png")));
+	}
+
+	public Flower(String id, 
+			int delay, 
+			float range, 
+			int energyDelay, 
+			int cost, 
+			int waterEvolQty, 
+			int airProd, 
+			int waterRegressQty, 
+			int energyRegressQty, 
+			ArrayList<Evolution> evolutions, 
+			int imageReference,
+			Image image){
+		super(id+"_Flower", delay, range, energyDelay, cost, waterEvolQty, airProd, waterRegressQty, energyRegressQty, evolutions, imageReference, image);
+	}
+}
