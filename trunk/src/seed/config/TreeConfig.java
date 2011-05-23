d9201945e97c3628da0d0e3458f4d9e71275a212
thierry.tang13@gmail.com <thierry.tang13@gmail.com@02c11bdb-3b26-27e6-09f1-825876d00b7d>
2011-05-23 22:58:08 +0000

    équillibrage

--- trunk/src/seed/config/TreeConfig.java
+++ trunk/src/seed/config/TreeConfig.java
@@ -12,7 +12,7 @@ public class TreeConfig extends PlantConfig {
 			properties.setProperty("RANGE", "100");
 			properties.setProperty("ENERGYDELAY", "400");
 			properties.setProperty("COST", "500");
-			properties.setProperty("WATEREVOLQTY", "3");
+			properties.setProperty("WATEREVOLQTY", "15");
 			properties.setProperty("AIRPROD", "100");
 			properties.setProperty("WATERREGRESSQTY", "0");
 			properties.setProperty("ENERGYREGRESSQTY", "-2");
@@ -22,45 +22,45 @@ public class TreeConfig extends PlantConfig {
 		}
 		case(1): //création d'un TreeConfig LVL1
 		{
-			properties.setProperty("EVOLCOST", "15");
+			properties.setProperty("EVOLCOST", "40");
 			properties.setProperty("DELAY", "200");
 			properties.setProperty("RANGE", "200");
 			properties.setProperty("ENERGYDELAY", "200");
 			properties.setProperty("COST", "500");
-			properties.setProperty("WATEREVOLQTY", "5");
-			properties.setProperty("AIRPROD", "200");
+			properties.setProperty("WATEREVOLQTY", "25");
+			properties.setProperty("AIRPROD", "300");
 			properties.setProperty("WATERREGRESSQTY", "0");
-			properties.setProperty("ENERGYREGRESSQTY", "-2");
+			properties.setProperty("ENERGYREGRESSQTY", "-5");
 			properties.setProperty("IMAGEREFERENCE", "6");
 			
 			break;
 		}
 		case(2): //création d'un TreeConfig LVL2
 		{
-			properties.setProperty("EVOLCOST", "25");
+			properties.setProperty("EVOLCOST", "50");
 			properties.setProperty("DELAY", "100");
 			properties.setProperty("RANGE", "250");
 			properties.setProperty("ENERGYDELAY", "75");
 			properties.setProperty("COST", "500");
-			properties.setProperty("WATEREVOLQTY", "10");
-			properties.setProperty("AIRPROD", "500");
+			properties.setProperty("WATEREVOLQTY", "35");
+			properties.setProperty("AIRPROD", "600");
 			properties.setProperty("WATERREGRESSQTY", "5");
-			properties.setProperty("ENERGYREGRESSQTY", "-2");
+			properties.setProperty("ENERGYREGRESSQTY", "-5");
 			properties.setProperty("IMAGEREFERENCE", "7");
 			
 			break;
 		}
 		case(3): //création d'un TreeConfig LVL3
 		{
-			properties.setProperty("EVOLCOST", "35");
+			properties.setProperty("EVOLCOST", "50");
 			properties.setProperty("DELAY", "100");
 			properties.setProperty("RANGE", "250");
 			properties.setProperty("ENERGYDELAY", "50");
 			properties.setProperty("COST", "500");
-			properties.setProperty("WATEREVOLQTY", "25");
-			properties.setProperty("AIRPROD", "700");
+			properties.setProperty("WATEREVOLQTY", "70");
+			properties.setProperty("AIRPROD", "1500");
 			properties.setProperty("WATERREGRESSQTY", "10");
-			properties.setProperty("ENERGYREGRESSQTY", "-2");
+			properties.setProperty("ENERGYREGRESSQTY", "-10");
 			properties.setProperty("IMAGEREFERENCE", "8");
 			
 			break;
