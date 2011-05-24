6d81e30aa7c191a976102fe62531edd94c20c9ee
guiguiduj@gmail.com <guiguiduj@gmail.com@02c11bdb-3b26-27e6-09f1-825876d00b7d>
2011-05-24 11:45:37 +0000

    ajout d'une barre de progression plutot que d'une valeur numérique pour la pollution

--- trunk/src/seed/interfaces/RessourceRenderComponent.java
+++ trunk/src/seed/interfaces/RessourceRenderComponent.java
@@ -1,5 +1,6 @@
 package seed.interfaces;
 
+import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.state.StateBasedGame;
@@ -18,10 +19,19 @@ public class RessourceRenderComponent extends RenderComponent {
 	public void render(GameContainer gc, StateBasedGame sb, Graphics gr) {
 		try
 		{
+			Graphics rectangle = new Graphics(); 
 			if(owner instanceof Ressource)
 			{
 				gr.drawString("Air : "+String.valueOf(((Ressource) owner).getAir()), 710, 0);
-				gr.drawString("Pol : "+String.valueOf(((Ressource) owner).getPollution()), 710, 20);
+				gr.drawString("Pol : "/*+String.valueOf(((Ressource) owner).getPollution())*/, 710, 20);
+				if(((Ressource) owner).getPollution() > 10050)
+					rectangle.setColor(Color.red);
+				else if(((Ressource) owner).getPollution() < 300)
+					rectangle.setColor(Color.blue);
+				else rectangle.setColor(Color.orange);
+				rectangle.fillRect(760, 25, 70, 10);
+				rectangle.setColor(Color.gray);
+				rectangle.fillRect(761, 26, ((float)((Ressource) owner).getPollution())*75f/12000f, 8);
 			}
 			else 
 				throw new Exception("RessourceRenderComponent ne s'applique qu'à l'Entity Ressource");
